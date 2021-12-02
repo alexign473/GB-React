d79a40e1c6ad17a836158dae6c44082d2161ec7a
Alexander Ignatev <alexign473@gmail.com>
2021-12-02 19:28:39 +0300

    lesson 7 upd

--- src/store/store.js
+++ src/store/store.js
@@ -5,18 +5,51 @@ import chatReducer from './chat/chatSlice'
 import logger from 'redux-logger';
 import { timeScheduler } from './middleware/timeScheduler '
 import { botAnswer } from './middleware/botAnswer'
+import { combineReducers } from 'redux';
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    profile: profileReducer,
+    chat: chatReducer,
+    theme: themeReducer
+});
+
+// 4. Установить и настроить redux-persist.
+// persist config obj
+// blacklist a store attribute using it's reducer name. Blacklisted attributes will not persist. (I did not find a way to blacklist specific values)
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    blacklist: ['chat'], //blacklisting a store attribute name, will not persist that store attribute.
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // 1. Установить и подключить к стору redux-thunk.
-// подключен по умолчанию
+
 export const store = configureStore({
-    reducer: {
-        profile: profileReducer,
-        chat: chatReducer,
-        theme: themeReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+    reducer: persistedReducer,
+    // middleware option needs to be provided for avoiding the error. ref: https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }).concat(
         logger,
         timeScheduler,
         botAnswer
     ),
 })
+
+export const persistor = persistStore(store)
